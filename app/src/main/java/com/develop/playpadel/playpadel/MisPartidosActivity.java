d432694a3f68efeab41318f6a68a4eff51b3413f
pvinals <pvinals94@gmail.com>
2017-04-17 17:40:48 +0200

    Creacion de todas las activities

---
+++ app/src/main/java/com/develop/playpadel/playpadel/MisPartidosActivity.java
@@ -0,0 +1,13 @@
+package com.develop.playpadel.playpadel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MisPartidosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mis_partidos2);
+    }
+}
